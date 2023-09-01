ab0e9f2de432a8b5fe9af63b8f65fc79d87d2e54
MahiiTech <maheshsatre16@gmail.com>
2023-09-01 20:22:23 +0530

    Mahesh : Added Databinding in Project

--- app/src/main/java/com/example/onlineassignmentproblem/UI/MainActivity.kt
+++ app/src/main/java/com/example/onlineassignmentproblem/UI/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.onlineassignmentproblem.UI
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.example.onlineassignmentproblem.R
+import com.example.onlineassignmentproblem.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.lifecycleOwner = this
+
+
     }
 }
