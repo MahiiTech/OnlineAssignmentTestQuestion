9743546ef35c085adf8ae37e05f85bec2da1404f
MahiiTech <maheshsatre16@gmail.com>
2023-09-01 19:49:42 +0530

    Mahesh : Splash Screen Code Added with CoroutineScope

--- app/src/main/java/com/example/onlineassignmentproblem/SplashScreen.kt
+++ app/src/main/java/com/example/onlineassignmentproblem/SplashScreen.kt
@@ -1,11 +1,22 @@
 package com.example.onlineassignmentproblem
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.onlineassignmentproblem.UI.MainActivity
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashScreen : AppCompatActivity() {
+    private val splashDelay: Long = 2000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+        GlobalScope.launch {
+            delay(splashDelay)
+            startActivity(Intent(this@SplashScreen, MainActivity::class.java))
+            finish()
+        }
     }
 }
